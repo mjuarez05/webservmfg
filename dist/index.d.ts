722dfed90f064e68b18f39da7ff84709bf37dcd8
Maximiliano Juarez <maximiliano.juarez@marfrig.com>
2021-09-21 12:10:45 -0300

    dist

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import { MarfrigWebAppComponent } from './application';
+import { ApplicationConfig } from '@loopback/core';
+export { MarfrigWebAppComponent };
+export declare function main(options?: ApplicationConfig): Promise<MarfrigWebAppComponent>;
